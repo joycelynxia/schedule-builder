d564035d50cb831e5cc33f0b2053a6ed200ae60f
Joycelyn Xia <joycelyn.xia@stonybrook.edu>
2026-02-13 21:45:37 -0500

    move to railway deployment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,3 @@
-// export const API_URL = "https://schedulr-twoi.onrender.com"
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiFetch = (url: string, options: RequestInit = {}) => {
